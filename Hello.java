2299a806b56f0bebea814126adc36676a995520d
sourav <souravgupta.it25@jecrc.ac.in>
2023-08-05 23:56:25 +0530

    hello file

---
+++ Hello.java
@@ -0,0 +1,40 @@
+package com.company;
+import java.util.Scanner;
+public class Hello {
+    public static void main(String[] args) {
+/*
+        Scanner s = new Scanner(System.in);
+   */
+/*     int a = 6;
+                int x  = ++a * 8;
+        /*System.out.println(x);*//*
+
+     */
+/*  float a = 7/4*9/2;
+//        System.out.println(a);
+      *//*
+
+//        int a = 10;
+//        int b = a + 8;
+//        int c = b - 8;
+//        System.out.println(b);
+//        System.out.println(c);
+      */
+/*   char grade = 'b';
+         grade = (char)( grade + 8);
+        System.out.println(grade);
+        grade = (char)(grade - 8);
+        System.out.println(grade);*//*
+
+        int  a = s.nextInt();
+        System.out.println(a>8);
+*/
+ int v = 8;
+ int u = 5;
+ int a = 3;
+ int s = 1;
+ int z = ((v*v)-(u*u))/(2*a*s);
+        System.out.println(z);
+
+    }
+}
